0b9cbc9b8c3f6e4770d0ef5a460f1355dd2b7338
Felipe Netto <lipecfc.barrella@hotmail.com>
2017-06-07 18:06:01 +0000

    alterando bases do foundation

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,13 +1,40 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE ViewPatterns               #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+
 module Foundation where
 
+import Yesod
+import Data.Text
+import Database.Persist
+import Data.Time.Calendar
 import Yesod.Core
+import Yesod.Static
+import Database.Persist.Postgresql
+    ( ConnectionPool, SqlBackend, runSqlPool, runMigration )
 
 data App = App
+    {
+        getStatic      :: Static,
+        connPool       :: ConnectionPool
+    }
 
 mkYesodData "App" $(parseRoutesFile "routes")
 
 instance Yesod App
+
+instance YesodPersist App where
+   type YesodPersistBackend App = SqlBackend
+   runDB f = do
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
