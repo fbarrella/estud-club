b9d46ff9994e7d32b90c745567ac7f1833f66bf9
Felipe Netto <lipecfc.barrella@hotmail.com>
2017-06-07 23:07:03 +0000

    adição do arquivo de usuário a stage area]

--- src/Usuario.hs
+++ src/Usuario.hs
@@ -9,3 +9,25 @@ import Control.Applicative
 import Data.Text
 
 import Database.Persist.Postgresql
+
+--getLoginR :: Handler Html
+--getLoginR =
+
+--postLoginR :: Handler Html
+--postLoginR =
+
+--getUsuariosR :: Handler Html
+--getUsuariosR =
+
+--postUsuariosR :: Handler Html
+--postUsuariosR = 
+
+--getEditaR :: Handler Html
+--getEditaR = 
+
+--postEditaR :: Handler Html
+--postEditaR = 
+
+--postLogoutR :: Handler Html
+--postLogoutR =
+
